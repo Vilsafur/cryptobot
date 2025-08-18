8b3ba88d47f9217f719b5d0364893c93a964d5a9
Vilsafur <vilsafur@gmail.com>
2025-08-18 20:46:51 +0000

    feat: Mise en place des pairs par défaut

--- src/migration/20250818_0002_init_pairs.sql
+++ src/migration/20250818_0002_init_pairs.sql
@@ -32,3 +32,10 @@ BEGIN
   SET updated_at = CAST(strftime('%s','now') AS INTEGER)
   WHERE pair = OLD.pair;
 END;
+
+-- Insertion des paires par défaut
+INSERT INTO pairs (pair, max_invest_fiat, max_per_tx_fiat, take_profit_pct, stop_loss_pct, created_at, updated_at)
+VALUES
+  ('XBT/EUR', 10, 5, 0.05, 0.03, CAST(strftime('%s','now') AS INTEGER), CAST(strftime('%s','now') AS INTEGER)),
+  ('ETH/EUR', 10, 5, 0.05, 0.03, CAST(strftime('%s','now') AS INTEGER), CAST(strftime('%s','now') AS INTEGER))
+ON CONFLICT DO NOTHING;  -- Ignore si déjà insérées
